3e61eca425ba06b3e5b43e02f2919fb52fa6b3e5
Khaled Al Moulla <khaled.almoulla@gmail.com>
2023-06-21 14:14:30 +0200

    ARVE update

--- arve/__init__.py
+++ arve/__init__.py
@@ -1,10 +1,10 @@
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__  = "Khaled Al Moulla"
 
-from .arve import ARVE
+from arve import ARVE
 
-from .arve import save
-from .arve import load
-from .arve import delete
+from arve import save
+from arve import load
+from arve import delete
 
 __all__ = ["ARVE", "save", "load", "delete"]
