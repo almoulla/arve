1503cb01de4f29be601273202f720c9ce2487167
Khaled Al Moulla <khaled.almoulla@gmail.com>
2023-10-12 13:06:44 +0200

    ARVE update

--- arve/star/__init__.py
+++ arve/star/__init__.py
@@ -1,8 +1,9 @@
-from .add_vpsd_components    import add_vpsd_components
-from .compute_vpsd           import compute_vpsd
-from .fit_vpsd_coefficients  import fit_vpsd_coefficients
-from .get_stellar_parameters import get_stellar_parameters
-from .plot_vpsd_components   import plot_vpsd_components
+from .add_vpsd_components                import add_vpsd_components
+from .compute_vpsd                       import compute_vpsd
+from .fit_vpsd_coefficients              import fit_vpsd_coefficients
+from .get_stellar_parameters             import get_stellar_parameters
+from .plot_vpsd_components               import plot_vpsd_components
+from .simulate_vrad_from_vpsd_components import simulate_vrad_from_vpsd_components
 
 from typing import Optional
 
@@ -11,7 +12,8 @@ class Star(
     compute_vpsd,
     fit_vpsd_coefficients,
     get_stellar_parameters,
-    plot_vpsd_components
+    plot_vpsd_components,
+    simulate_vrad_from_vpsd_components
     ):
     """ARVE Star sub-class.
     """
---
+++ arve/star/simulate_vrad_from_vpsd_components.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+class simulate_vrad_from_vpsd_components:
+
+    def simulate_vrad_from_vpsd_components(self, time_start:float, time_stop:float, time_step:float) -> None:
+        """Simulate radial velocities (RVs) from velocity power spectral density (VPSD) components.
+
+        :param time_start: start time
+        :type time_start: float
+        :param time_stop: stop time
+        :type time_stop: float
+        :param time_step: time step
+        :type time_step: float
+        :return: None
+        :rtype: None
+        """
+
+        # time parameters
+        time  = np.arange(time_start, time_stop, time_step)
+        T     = time_stop - time_start
+        dt    = time_step
+        Ntime = len(time)
+
+        # frequency parameters
+        freq  = np.arange(1/T, 1/(2*dt), 1/T)
+        df    = 1/T
+        omega = 2*np.pi*freq
+        Nfreq = len(freq)
+
+        # random phases
+        phi = np.random.uniform(-np.pi, np.pi, Nfreq)
+
+        # read VPSD components
+        vpsd_comp  = self.vpsd_components
+        comp_name  = list(vpsd_comp.keys())
+        Nvpsd_comp = len(vpsd_comp)
+
+        # empty array for vpsd
+        vpsd_arr = np.zeros((Nvpsd_comp,Nfreq))
+
+        # loop components
+        for i in range(Nvpsd_comp):
+
+            # component type and coefficient values
+            comp_type = vpsd_comp[comp_name[i]]["type"]
+            coef_val  = vpsd_comp[comp_name[i]]["coef_val"]
+
+            # evaluate component on frequencies
+            if comp_type == "Constant":
+                c0, = coef_val
+                vpsd_arr[i] = c0*np.ones_like(freq)
+            if comp_type == "Lorentz":
+                c0, c1, c2 = coef_val
+                vpsd_arr[i] = c0*c1**2/(c1**2+(freq-c2)**2)
+            if comp_type == "Harvey":
+                c0, c1, c2 = coef_val
+                vpsd_arr[i] = c0/(1+(c1*freq)**c2)
+        
+        # velocity amplitudes
+        vamp_arr = np.sqrt(vpsd_arr*df)
+
+        # empty array for RV components
+        vrad_comp = np.zeros((Ntime,Nvpsd_comp))
+
+        # loop times
+        for i in range(Ntime):
+            
+            # array of sine terms
+            sin_arr = np.sin(omega*time[i] + phi)
+
+            # loop components
+            for j in range(Nvpsd_comp):
+            
+                # simulated RV components
+                vrad_comp[i,j] = np.dot(vamp_arr[j], sin_arr)
+        
+        # dictionary with RV components
+        vrad_dict = {}
+        vrad_dict["time_val"] = time
+        for i in range(Nvpsd_comp):
+            vrad_dict[comp_name[i]] = vrad_comp[:,i]
+        
+        # save RV components
+        self.arve.data.vrad["vrad_comp"] = vrad_dict
+    
+        return None
