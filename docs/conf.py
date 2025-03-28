4bfd91013df2b5de2336ef6711cd8908a0552642
Khaled Al Moulla <khaled.almoulla@gmail.com>
2025-03-28 17:07:18 +0000

    ARVE update

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,14 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "ARVE"
 copyright = "2025, Khaled Al Moulla"
 author = "Khaled Al Moulla"
-release = "0.2.4"
+release = "0.2.8"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**/aux_data/**"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
