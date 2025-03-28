fdcf9f1c91fb14dd69650bb542d30344cff96b01
Khaled Al Moulla <khaled.almoulla@gmail.com>
2025-03-28 17:16:38 +0000

    ARVE update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as file:
 
 setup(
     name="arve",
-    version="0.2.8",
+    version="0.2.9",
     author="Khaled Al Moulla",
     author_email="khaled.almoulla@gmail.com",
     description="Analyzing Radial Velocity Elements",
